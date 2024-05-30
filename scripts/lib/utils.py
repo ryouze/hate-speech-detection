d1d06b9223c71fe6f60f056ab0aaa7260282dd63
ryouze <98982999+ryouze@users.noreply.github.com>
2024-05-30 21:05:14 +0200

    [lib] Add logging functions.

---
+++ scripts/lib/utils.py
@@ -0,0 +1,50 @@
+"""
+Module: utils.py
+
+Handles repeating code that is used in different parts of the project.
+"""
+
+from pathlib import Path as _Path
+from sys import stdout as _stdout
+from time import time as _time
+
+from loguru import logger as _logger
+
+# Public objects
+__all__: list[str] = [
+    # VSCode: Sort lines in descending order
+    "configure_logging_level",
+    "create_timestamped_log_file",
+]
+
+
+def configure_logging_level(
+    verbose: bool,
+) -> None:
+    """
+    Set the logging level based on the verbose flag.
+
+    Args:
+        verbose (bool): Flag to enable verbose logging. If True, set the log level to DEBUG, otherwise, set it to INFO.
+    """
+    if not verbose:
+        _logger.remove()
+        _logger.add(_stdout, level="INFO")
+
+
+def create_timestamped_log_file(
+    script_name: str,
+    output_directory: _Path,
+) -> None:
+    """
+    Initialize a logger that logs to a timestamped log file in the output directory.
+
+    E.g., "~/logs/train_1717095105.731416.log".
+
+    Args:
+        script_name (str): Name of the script (e.g., "train.py").
+        output_directory (Path): Path to the output directory (e.g., "~/logs").
+    """
+    # Strip the file extension from the script name (e.g., "~/scripts/train.py" -> "train"), add the current time in seconds since the Epoch to the log file name, add ".log" at the end
+    file_name: str = f"{_Path(script_name).stem}_{_time()}.log"
+    _logger.add(str(output_directory / file_name))
